ed58f243d589153d620bc851546a695099291417
Hsing-Yu Chen <34529973+chsiny@users.noreply.github.com>
2024-11-08 22:20:51 +1000

    modify github link

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -1,46 +1,3 @@
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-// import projectsData from '../data/projects.json'; // Import the same project data
-// import './ProjectDetail.css'; // Import custom styles for this page
-
-// const ProjectDetail = () => {
-//   const { id } = useParams(); // Get the project ID from the URL
-//   const project = projectsData.find((proj) => proj.id === id); // Find the project by ID
-
-//   if (!project) {
-//     return <div>Project not found!</div>; // Handle invalid project ID
-//   }
-
-//   return (
-//     <div className="project-detail">
-//       {/* Project Name and Brief Description */}
-//       <h1>{project.name}</h1>
-//       <p className="brief-description">{project.description}</p>
-
-//       {/* Skills Used */}
-//       <p><strong>Tech stack:</strong> {project.skills}</p>
-
-//       {/* Image Gallery Section */}
-//       <section className="image-gallery">
-//         <div className="images-container">
-//           {project.images && project.images.length > 0 ? (
-//             project.images.map((imgSrc, index) => (
-//               <img key={index} src={imgSrc} alt={`${project.name} ${index + 1}`} className="project-image" />
-//             ))
-//           ) : (
-//             <p>No images available for this project.</p>
-//           )}
-//         </div>
-//       </section>
-
-//       {/* Detailed Description Section */}
-//       <section className="detailed-description">
-//         <p>{project.detailedDescription || "To be continued"}</p>
-//       </section>
-//     </div>
-//   );
-// };
-
 // export default ProjectDetail;
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
@@ -82,11 +39,11 @@ const ProjectDetail = () => {
       {project.github ? (
         <p>
           <strong>Repository: </strong>
-          <a href={project.github}>View on GitHub</a>
+          <a href={project.github} target="_blank" rel="noopener noreferrer">
+            View on GitHub
+          </a>
         </p>
-      ) : (
-        <p></p>
-      )}
+      ) : null}
 
       {/* Image Gallery Section */}
       <section className="image-gallery">
